65ad3ca739154a0ac225c2731f7872e93038d396
Uday Bute <82996279+udaybute@users.noreply.github.com>
2023-09-22 17:50:21 +0530

    Update App.js
    done

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const apidata = "https://fakestoreapi.com/products/";
@@ -10,40 +10,34 @@ const App = () => {
     const getData = async () => {
       const response = await axios.get(apidata);
       setData(response.data);
-    }
+    };
 
     getData();
-  }, [])
+  }, []);
+
   return (
     <>
       <h1> Fake Store Data</h1>
       <div className='container p-2 m-2'>
-              <div className='row'>
-      {
-        alldata.map((post) => {
-          const { id, title, image, description } = post;
-          return (
-           
-                <div className='col-4'>
-                <div class="card m-2" key={id} style={{ width: "400px", height:"500px"}}>
-                  <img src={image} class="card-img-top img-fluid w-50 p-2" alt="..." />
-                  <div class="card-body d-flex">
-                    <h5 class="card-title">{title}</h5>
-                    <p class="card-text">{description}</p>
+        <div className='row'>
+          {alldata.map((post) => {
+            const { id, title, image, description } = post;
+            return (
+              <div className='col-4' key={id}>
+                <div className="card m-2 d-flex flex-column h-100">
+                  <img src={image} className="card-img-top img-fluid w-50 p-2" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{description}</p>
                   </div>
                 </div>
-                </div>
-           
-          )
-
-        })
-      }
-         </div>
-            </div>
-
-
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
